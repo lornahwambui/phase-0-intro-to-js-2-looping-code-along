8201128686f182820f9667b4e2f0b88a5090915a
lornahWambuiGitau <lornah.gitau@student.moringaschool.com>
2023-09-22 18:37:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    const thankYouMessages = [];
+ for (let i = 0; i < namesArray.length; i++) {
+      const message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+     thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+  const names = ["Alice", "Bob", "Charlie"];
+  const event = "birthday";
+  
+  const messages = writeCards(names, event);
+  for (const message of messages) {
+    console.log(message);
+  }
+
+  function countDown(startingNumber) {
+    while (startingNumber >= 0) {
+      console.log(startingNumber);
+      startingNumber--; 
+    }
+  }
+  countDown(10);
+  
